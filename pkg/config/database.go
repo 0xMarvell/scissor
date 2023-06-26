ce8328696dec4d2060def03320cf65fdf23d1b9c
0xMarvell <rokemarvellous@gmail.com>
2023-06-26 11:27:25 +0100

    optimize for deployment

--- pkg/config/database.go
+++ pkg/config/database.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"log"
+	"os"
 
 	"github.com/0xMarvell/scissor/pkg/models"
 	"github.com/0xMarvell/scissor/pkg/utils"
@@ -16,7 +17,7 @@ func Connect() {
 	var dbConnectErr error
 
 	// dsn := "host=db user=postgres password=postgres dbname=scissor port=5432 sslmode=disable TimeZone=Africa/Lagos" // for development
-	dsn := "postgres://scissor_db_service_user:XZzvqtu77yCIMQwfJl3i9UBOhvgsFEmx@dpg-cicm7n59aq03rjn42to0-a.oregon-postgres.render.com/scissor_db_service" // for deployment
+	dsn := os.Getenv("DSN") // for deployment
 
 	DB, dbConnectErr = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	utils.CheckDbErr("error connecting to database: ", dbConnectErr)
